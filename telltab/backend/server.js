ac2b2dad4691425151847f6aae32d1496ac7ddc0
Matthew Kongsiri <mkong@seas.upenn.edu>
2019-06-16 13:04:10 -0400

    Update and delete routes for all db classes

--- telltab/backend/server.js
+++ telltab/backend/server.js
@@ -70,9 +70,55 @@ router.get('/getCompany', (req, res) => {
 // this is our update method
 // this method overwrites existing data in our database
 router.post('/updateData', (req, res) => {
-  const { id, update } = req.body;
-  Data.findByIdAndUpdate(id, update, (err) => {
+  const { id, message } = req.body;
+
+  Database.Data.findByIdAndUpdate(id, {$set: {message: message}}, {new: true}, (err, model) => {
     if (err) return res.json({ success: false, error: err });
+    console.log(model);
+    return res.json({ success: true });
+  });
+});
+
+router.post('/updateFeedback', (req, res) => {
+  const { id, content, votes, progress, admin, category, tags,
+    priority, visibility, resolved } = req.body;
+
+  Database.Feedback.findByIdAndUpdate(id, {$set: {content: content, votes: votes,
+    progress: progress, admin: admin, category: category, tags: tage, priority: priotiy,
+    visibility: visibility, resolved: resolved}}, {new: true}, (err, model) => {
+    if (err) return res.json({ success: false, error: err });
+    console.log(model);
+    return res.json({ success: true });
+  });
+});
+
+router.post('/updateGroup', (req, res) => {
+  const { id, name, users } = req.body;
+
+  Database.Group.findByIdAndUpdate(id, {$set: {name: name, users: users}}, {new: true}, (err, model) => {
+    if (err) return res.json({ success: false, error: err });
+    console.log(model);
+    return res.json({ success: true });
+  });
+});
+
+router.post('/updateComment', (req, res) => {
+  const { id, content } = req.body;
+
+  Database.Comment.findByIdAndUpdate(id, {$set: {content: content}}, {new: true}, (err, model) => {
+    if (err) return res.json({ success: false, error: err });
+    console.log(model);
+    return res.json({ success: true });
+  });
+});
+
+router.post('/updateCompany', (req, res) => {
+  const { id, companyName, config, tags } = req.body;
+
+  Database.Company.findByIdAndUpdate(id, {$set: {companyName: companyName, config: config,
+    tags: tags}}, {new: true}, (err, model) => {
+    if (err) return res.json({ success: false, error: err });
+    console.log(model);
     return res.json({ success: true });
   });
 });
@@ -81,7 +127,39 @@ router.post('/updateData', (req, res) => {
 // this method removes existing data in our database
 router.delete('/deleteData', (req, res) => {
   const { id } = req.body;
-  Data.findByIdAndRemove(id, (err) => {
+  Database.Data.findByIdAndRemove(id, (err) => {
+    if (err) return res.send(err);
+    return res.json({ success: true });
+  });
+});
+
+router.delete('/deleteFeedback', (req, res) => {
+  const { id } = req.body;
+  Database.Feedback.findByIdAndRemove(id, (err) => {
+    if (err) return res.send(err);
+    return res.json({ success: true });
+  });
+});
+
+router.delete('/deleteGroup', (req, res) => {
+  const { id } = req.body;
+  Database.Group.findByIdAndRemove(id, (err) => {
+    if (err) return res.send(err);
+    return res.json({ success: true });
+  });
+});
+
+router.delete('/deleteComment', (req, res) => {
+  const { id } = req.body;
+  Database.Comment.findByIdAndRemove(id, (err) => {
+    if (err) return res.send(err);
+    return res.json({ success: true });
+  });
+});
+
+router.delete('/deleteCompany', (req, res) => {
+  const { id } = req.body;
+  Database.Company.findByIdAndRemove(id, (err) => {
     if (err) return res.send(err);
     return res.json({ success: true });
   });
@@ -132,6 +210,66 @@ router.post('/putFeedback', (req, res) => {
   data.priority = priority;
   data.visibility = visibility;
   data.resolved = resolved;
+  data.save((err) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+router.post('/putGroup', (req, res) => {
+  let data = new Database.Group();
+
+  const { name, users } = req.body;
+  console.log(req.body)
+
+  if (!name) {
+    return res.json({
+      success: false,
+      error: 'INVALID INPUTS',
+    });
+  }
+  data.name = name;
+  data.users = users;
+  data.save((err) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+router.post('/putComment', (req, res) => {
+  let data = new Database.Comment();
+
+  const { content } = req.body;
+  console.log(req.body)
+
+  if (!content) {
+    return res.json({
+      success: false,
+      error: 'INVALID INPUTS',
+    });
+  }
+  data.content = content;
+  data.save((err) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+router.post('/putCompany', (req, res) => {
+  let data = new Database.Company();
+
+  const { companyName, config, tags } = req.body;
+  console.log(req.body)
+
+  if (!companyName) {
+    return res.json({
+      success: false,
+      error: 'INVALID INPUTS',
+    });
+  }
+  data.companyName = companyName;
+  data.config = config;
+  data.tags = tags;
   data.save((err) => {
     if (err) return res.json({ success: false, error: err });
     return res.json({ success: true });

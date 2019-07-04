a3ca7b094c62d05e920048f7fb3c3dfd522c3629
Matthew Kongsiri <mkong@seas.upenn.edu>
2019-07-03 21:17:47 -0400

    Configured elastic search

--- telltab/backend/models/Comment.js
+++ telltab/backend/models/Comment.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const mongoosastic = require("mongoosastic")
+const elasticsearch = require("elasticsearch")
 const Schema = mongoose.Schema;
 const { ObjectId } = Schema.Types;
 
@@ -13,6 +15,22 @@ var commentSchema = new Schema({
 	content: String
 });
 
+var esClient = new elasticsearch.Client({host: 'https://tr0wmngsvx:sv307a66pr@tt-5489597012.us-east-1.bonsaisearch.net:443'});
+
+commentSchema.plugin(mongoosastic, {
+	esClient: esClient
+});
+
 var Comment = mongoose.model("Comment", commentSchema);
+
+Comment.createMapping(function(err, mapping){  
+	if(err){
+	  console.log('error creating mapping');
+	  console.log(err);
+	}else{
+	  console.log('mapping created!');
+	  console.log(mapping);
+	}
+});
 
 module.exports = Comment;

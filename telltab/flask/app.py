ddc40ad04801cca1bb65eaefc44fa9da3d4d694a
Matthew Kongsiri <mkong@seas.upenn.edu>
2019-07-06 16:11:58 -0400

    Implemented basic functionality for autotag and similarity

--- telltab/flask/app.py
+++ telltab/flask/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from autotag import autotag
+from similarity import similarity
 
 import json
 
@@ -23,3 +24,14 @@ def get_autotag():
     if res == None:
         return json.dumps({'success': False, 'error': 'No auto tag found'})
     return json.dumps({'success': True, 'autotag': res})
+
+@app.route('/similarity', methods=['POST'])
+def get_similarity():
+    body = request.get_json()
+    if not('doc1' in body and 'doc2' in body):
+        return json.dumps({'success': False, 'error': 'Missing argument(s) in request body'})
+
+    doc1 = body['doc1']
+    doc2 = body['doc2']
+    res = similarity(doc1, doc2)
+    return json.dumps({'success': True, 'similarity': res})
---
+++ telltab/flask/similarity.py
@@ -0,0 +1,11 @@
+import re
+import nltk
+
+from nltk.corpus import stopwords
+
+def similarity(doc1, doc2):
+    stops = set(stopwords.words('english'))
+    l1 = set(re.sub("[^\w]", " ", doc1).split()) - stops
+    l2 = set(re.sub("[^\w]", " ", doc2).split()) - stops
+    
+    return len(l1.intersection(l2)) / len(l1.union(l2))

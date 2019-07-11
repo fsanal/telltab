16909d6a89f6fb5689730cc29dddc975d219e335
Matthew Kongsiri <mkong@seas.upenn.edu>
2019-07-11 17:47:47 -0400

    Backend routes for login

--- telltab/backend/routes/authentication_routes.js
+++ telltab/backend/routes/authentication_routes.js
@@ -3,11 +3,16 @@ const router = express.Router();
 const passport = require('passport');
 
 const local_authentication_controller = require('../controllers/authentication_controllers/LocalAuthentication');
-//router.post('/authenticate/login', local_authentication_controller.login);
+router.post('/authenticate/login', function(req, res, next) {
+    passport.authenticate('local-login', function(err, user) {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true, user: user });
+    })(req, res, next);
+});
 router.post('/authenticate/signup', function(req, res, next) {
     passport.authenticate('local-signup', function(err, user) {
-        if (err) return res.json({ success: false, eror: err });
-        return res.json({ success:true, user: user });
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true, user: user });
     })(req, res, next);
 });
 

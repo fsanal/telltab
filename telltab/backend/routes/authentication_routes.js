7a2a2c0cd50b9a3bc08b41b8adeefc8743beea8f
Matthew Kongsiri <mkong@seas.upenn.edu>
2019-07-14 11:20:18 -0400

    Implemented backend Facebook authentication

--- telltab/backend/routes/authentication_routes.js
+++ telltab/backend/routes/authentication_routes.js
@@ -23,4 +23,19 @@ router.post('/authenticate/signup', function(req, res, next) {
 });
 router.get('/authenticate/logout', local_authentication_controller.logout);
 
+const facebook_authentication_controller = require('../controllers/authentication_controllers/FacebookAuthentication');
+router.get('/authenticate/facebook', passport.authenticate('facebook', {
+    scope: ['public_profile', 'email']
+}));
+router.get('/authenticate/facebook/callback', function(req, res, next) {
+    passport.authenticate('facebook', function(err, user) {
+        if (err) return res.json({ success: false, error: err });
+        req.logIn(user, function(err) {
+            if (err) return res.json({ success: false, error: err });
+            return res.json({ success: true });
+        });
+    })(req, res, next);
+});
+router.get('/authenticate/facebook/logout', facebook_authentication_controller.logout);
+
 module.exports = router;
